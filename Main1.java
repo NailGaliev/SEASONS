77b7619bc43ddcb830412a1a2491a4ea716c989d
NailGaliev <55915785+NailGaliev@users.noreply.github.com>
2019-10-16 21:45:33 +0300

    homew

---
+++ Main1.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] a = new int[5][];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + "");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+
+
