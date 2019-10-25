92fc7ef4fee0a180b1b352f5c12716084f341ffb
NailGaliev <55915785+NailGaliev@users.noreply.github.com>
2019-10-25 18:15:17 +0300

    table

---
+++ Table.java
@@ -0,0 +1,24 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Table {
+
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        System.out.print("Видите размер таблицы по вертикали - ");
+        int n = r.nextInt();
+        System.out.print("Видите размер таблицы по горизонтали - ");
+        int l = r.nextInt();
+        for (int i = 1; i < n + 1; i++) {
+            for (int k = 1; k < l + 1; k++) {
+                System.out.printf("%4d", (k * i));}
+            System.out.println("");
+        }
+    }
+
+}
+
+
+
