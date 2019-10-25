0a9b280964c61f0a192b648d45e57b226a70334f
NailGaliev <55915785+NailGaliev@users.noreply.github.com>
2019-10-25 18:00:57 +0300

    Update Main.java
    month

--- Main.java
+++ Main.java
@@ -3,14 +3,53 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] a = new int[5][];
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.print(a[i][j] + "");
-            }
-            System.out.println();
+      Scanner sc= new Scanner(System.in);
+  int x =150;
+        System.out.print("enter month");
+  x = sc.nextInt();
+
+
+        switch (x){
+
+            case 1:
+                System.out.print("январь");
+                break;
+            case 2:
+                System.out.print("февраль");
+                break;
+            case 3:
+                System.out.print("март");
+                break;
+            case 4:
+                System.out.print("апрель");
+                break;
+            case 5:
+                System.out.print("май");
+                break;
+            case 6:
+                System.out.print("июнь");
+                break;
+            case 7:
+                System.out.print("июль");
+                break;
+            case 8:
+                System.out.print("август");
+                break;
+            case 9:
+                System.out.print("сентябрь");
+                break;
+            case 10:
+                System.out.print("октябрь");
+                break;
+            case 11:
+                System.out.print("ноябрь");
+                break;
+            case 12:
+                System.out.print("декабрь");
+                break;
         }
-    }
+        }
+
 }
 
 
